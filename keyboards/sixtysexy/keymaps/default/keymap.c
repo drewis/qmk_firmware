2301185c9ca56fa7e6be8f323234c9e7995dfbc6
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2018-02-10 22:50:57 -0600

    add the sixtysexy
    The sixtysexy is a dumb name for a 66 key hybrid ergodox/atreus.
    It was designed by me to fill my unique requirements. the major
    feature is an additional column of the top three rows of the right
    hand giving us back the [] /\ -= pairs than usually reside there
    (on dvorak) removing the need to learn a new location for these keys
    used quite frequenly in programming, also makes it where f12 is on
    the top row (on L2). The second major feature is the size, it is designed
    to fit perfectly on top of the 2015 MBP keyboard, add some wrist wrests and
    you have a pretty nice setup that keeps the trackpad in the familiar place

---
+++ keyboards/sixtysexy/keymaps/default/keymap.c
@@ -0,0 +1,51 @@
+/* Copyright 2018 Andrew Sutherland (dr3wsuth3rland@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "sixtysexy.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	KEYMAP(
+		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_LBRC, KC_RBRC, 
+		KC_NO, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_ESC, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH, KC_BSLS, 
+		KC_LCTL, KC_A, KC_O, KC_E, KC_U, KC_I, KC_ENT, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS, KC_EQL, 
+		KC_LSFT, KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_TAB, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT, 
+		OSL(1), KC_NO, KC_NO, KC_LALT, KC_LGUI, KC_SPC, KC_BSPC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_RCTL),
+
+	KEYMAP(
+		KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_HOME, KC_PGDN, KC_PGUP, KC_PGDN, KC_TRNS),
+
+	KEYMAP(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+	/*
+	switch (id) {
+
+	}
+	*/
+	return MACRO_NONE;
+}
+
---
+++ keyboards/sixtysexy/readme.md
@@ -0,0 +1,15 @@
+# sixtysexy
+
+![sixtysexy](imgur.com image replace me!)
+
+A short description of the keyboard/project
+
+Keyboard Maintainer: [drewis](https://github.com/drewis)  
+Hardware Supported: The PCBs, controllers supported  
+Hardware Availability: links to where you can find this hardware
+
+Make example for this keyboard (after setting up your build environment):
+
+    make sixtysexy:default
+
+See [build environment setup](https://docs.qmk.fm/build_environment_setup.html) then the [make instructions](https://docs.qmk.fm/make_instructions.html) for more information.
