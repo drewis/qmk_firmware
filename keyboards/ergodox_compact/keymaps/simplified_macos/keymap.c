3474f57ffdb9a03246282e62b848b44dffaaab72
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2018-11-23 23:53:20 -0600

    move rctrl up

--- keyboards/ergodox_compact/keymaps/simplified_macos/keymap.c
+++ keyboards/ergodox_compact/keymaps/simplified_macos/keymap.c
@@ -33,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                 KC_NO  ,KC_NO  ,KC_NO  ,KC_NO ,KC_NO  ,KC_NO  ,
 		KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                    KC_F,   KC_G,   KC_C,   KC_R,  KC_L,   KC_BSPC,
 		LCTL_T(KC_ESC),
-		         KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                    KC_D,   KC_H,   KC_T,   KC_N,  KC_S,   KC_MINS,
+		         KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                    KC_D,   KC_H,   KC_T,   KC_N,  KC_S,   RCTL_T(KC_MINS),
 		KC_LSPO, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X, OSL(2), OSL(1),    KC_B,   KC_M,   KC_W,   KC_V,  KC_Z,   KC_RSPC,
-		KC_CAPS, KC_SLSH, KC_DEL,  KC_LALT, KC_LGUI, MO(1),KC_SPC, KC_ENT,    MO(2),  KC_LEFT,KC_DOWN,KC_UP, KC_RGHT,RCTL_T(KC_DEL)),
+		KC_CAPS, KC_NO, KC_SLSH,  KC_LALT, KC_LGUI, MO(1),KC_SPC, KC_ENT,    MO(2),  KC_LEFT,KC_DOWN,KC_UP, KC_RGHT,KC_DEL),
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
