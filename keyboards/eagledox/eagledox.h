f513e5f22c85d4c1f254fce1faafa71f56d0471d
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2018-11-26 22:36:36 -0600

    add eagledox

---
+++ keyboards/eagledox/eagledox.h
@@ -0,0 +1,48 @@
+/* Copyright 2018 REPLACE_WITH_YOUR_NAME
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef EAGLEDOX_H
+#define EAGLEDOX_H
+
+#include "quantum.h"
+
+/* This a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+
+#define LAYOUT( \
+    k11, k12, k21, k22, k31, k32,           k41, k42, k51, k52, k61, k62, \
+    k13, k14, k23, k24, k33, k34, k39, k49, k43, k44, k53, k54, k63, k64,    \
+    k15, k16, k25, k26, k35, k36, k29, k59, k45, k46, k55, k56, k65, k66,   \
+    k17, k18, k27, k28, k37, k38, k19, k69, k47, k48, k57, k58, k67, k68   \
+) \
+{ \
+    { k11, k21, k31, k41, k51, k61 }, \
+    { k12, k22, k32, k42, k52, k62 }, \
+    { k13, k23, k33, k43, k53, k63 }, \
+    { k14, k24, k34, k44, k54, k64 }, \
+    { k15, k25, k35, k45, k55, k65 }, \
+    { k16, k26, k36, k46, k56, k66 }, \
+    { k17, k27, k37, k47, k57, k67 }, \
+    { k18, k28, k38, k48, k58, k68 }, \
+    { k19, k29, k39, k49, k59, k69 }, \
+}
+
+#endif
---
+++ keyboards/eagledox/keymaps/macos/keymap.c
@@ -0,0 +1,118 @@
+/* Copyright 2018 REPLACE_WITH_YOUR_NAME
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+#define _______ KC_TRNS
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  QMKBEST = SAFE_RANGE,
+  QMKURL
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Dvorak
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  /   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[0] = LAYOUT( /* Base */
+		KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                    KC_F,   KC_G,   KC_C,   KC_R,  KC_L,   KC_BSPC,
+		LCTL_T(KC_ESC),
+		         KC_A,    KC_O,    KC_E,    KC_U,    KC_I,  KC_NO,  KC_NO,    KC_D,   KC_H,   KC_T,   KC_N,  KC_S,   RCTL_T(KC_MINS),
+		KC_LSPO, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,  OSL(2), OSL(1),   KC_B,   KC_M,   KC_W,   KC_V,  KC_Z,   KC_RSPC,
+		KC_CAPS, KC_NO,   KC_SLSH, KC_LALT, KC_LGUI, MO(1), KC_SPC, KC_ENT,   MO(2),  KC_LEFT,KC_DOWN,KC_UP, KC_RGHT,KC_DEL),
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[1] = LAYOUT( /* L1 */
+		KC_GRV ,KC_1,      KC_2,      KC_3,      KC_4,      KC_5,                      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSPC,
+		_______,KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     _______,_______,_______,KC_PLUS,KC_LCBR,KC_RCBR,_______,KC_PIPE,
+		_______,LGUI(KC_Z),LGUI(KC_X),LGUI(KC_C),LGUI(KC_V),LGUI(KC_A),_______,_______,_______,KC_EQL, KC_LBRC,KC_RBRC,_______,_______,
+		_______,_______,   _______,   _______,   _______,   _______,   _______,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END, _______),
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[2] = LAYOUT( /* L2 */
+		KC_TILD,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,                KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,
+		_______,KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, _______,_______,_______,KC_EXLM,KC_LCBR,KC_RCBR,_______,KC_BSLS,
+		_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_EQL ,KC_LBRC,KC_RBRC,_______,_______,
+		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______),
+[3] = LAYOUT( /* L3 */
+		_______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
+		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______),
+};
+
+
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QMKBEST:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING("QMK is the best thing ever!");
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case QMKURL:
+      if (record->event.pressed) {
+        // when keycode QMKURL is pressed
+        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
+      } else {
+        // when keycode QMKURL is released
+      }
+      break;
+  }
+  return true;
+}
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
