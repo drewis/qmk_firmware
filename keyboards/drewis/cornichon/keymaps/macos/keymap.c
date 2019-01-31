aa986ce79dea4990167ea1bb1a202da6e3bc979c
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2019-01-31 09:47:00 -0600

    cornichon:dedicate bspc

--- keyboards/drewis/cornichon/keymaps/macos/keymap.c
+++ keyboards/drewis/cornichon/keymaps/macos/keymap.c
@@ -26,7 +26,7 @@
 #define BSLS_CTL RCTL_T(KC_BSLS)
 #define PIPE_CTL RCTL_T(KC_PIPE)
 #define DEL_ALT LGUI_T(KC_DEL)
-#define BAK_ALT LGUI_T(KC_BSPC)
+//#define BAK_ALT LGUI_T(KC_BSPC)
 #define SPTLITE LSFT(KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_QUOT, KC_COMM, KC_DOT,  KC_P  , KC_Y  , KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,
   KC_A,    KC_O,    KC_E,    KC_U,   KC_I,   KC_D,   KC_H,   KC_T,   KC_N,   KC_S,
   KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,
-  ESC_CTL, DEL_ALT, KC_LSFT, MO(1),  KC_SPC, KC_ENT, MO(2),  KC_RSFT,BAK_ALT,MIN_CTL
+  ESC_CTL, DEL_ALT, KC_LSFT, MO(1),  KC_SPC, KC_ENT, MO(2),  KC_RSFT,KC_BSPC,MIN_CTL
   ),
 /* Raise
  * ,---------------------------------------------------------------------.
