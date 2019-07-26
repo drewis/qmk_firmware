d74c282fc23641cd76765cf70479d6393cd98dbe
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2019-07-25 18:47:06 -0600

    kestrel: dc: disable function keys

--- keyboards/drewis/kestrel/keymaps/devilscanyon/keymap.c
+++ keyboards/drewis/kestrel/keymaps/devilscanyon/keymap.c
@@ -47,10 +47,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [0] = LAYOUT( /* Base */
-		KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,  KC_F,   KC_G,   KC_C,   KC_R,  KC_L,   KC_BSPC,
-		CTL_ESC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,  KC_D,   KC_H,   KC_T,   KC_N,  KC_S,   CTL_MIN,
-		KC_LEFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,  KC_B,   KC_M,   KC_W,   KC_V,  KC_Z,   KC_RIGHT,
-	        KC_DEL,  KC_LALT, KC_LGUI, RAISE,   KC_SPC,  KC_LSFT,KC_RSFT,KC_ENT,LOWER,  KC_DOWN,KC_UP, KC_CAPS),
+		KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_BSPC,
+		CTL_ESC, KC_A,    KC_O,    KC_E,    KC_U,   KC_I,   KC_D,   KC_H,   KC_T,   KC_N,   KC_S,   CTL_MIN,
+		KC_LEFT, KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,   KC_RIGHT,
+	        KC_DEL,  KC_LALT, KC_LGUI, KC_SPC,  RAISE,  KC_LSFT,KC_RSFT,LOWER,  KC_ENT, KC_DOWN,KC_UP,  KC_CAPS),
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
@@ -63,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [1] = LAYOUT( /* L1 */
-		KC_GRV ,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, _______,
-		_______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,  KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_PIPE,
-		_______,C_Z,    C_X,    C_C,    C_V,    C_A,   KC_PLUS,KC_EQL, KC_RABK,KC_LBRC,KC_RBRC,_______,
-		_______,_______,_______,_______,_______,_______,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END),
+		KC_GRV ,_______,_______,_______,KC_NO,  KC_NO, KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_QUES,_______,
+		_______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,  KC_6,    KC_7,   KC_8,   KC_9,   KC_0,   KC_PIPE,
+		KC_HOME,C_Z,    C_X,    C_C,    C_V,    C_A,   KC_PLUS, KC_EQL, KC_RABK,KC_LBRC,KC_RBRC,KC_END,
+		_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_PGDN,KC_PGUP,_______),
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |  F11 |  F12 |  F13 | F14  |  F15 |             |  F16 |  F17 | F18  |   ?  |  /   |
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [2] = LAYOUT( /* L2 */
-		KC_TILD,KC_F11, KC_F12, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_QUES,KC_SLSH,_______,
+		KC_TILD,_______,_______,_______,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO  ,KC_SLSH,_______,
 		_______,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSLS,
 		_______,_______,_______,_______,_______,_______,KC_EXLM,KC_EQL, KC_LABK,KC_LCBR,KC_RCBR,_______,
 		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______),
