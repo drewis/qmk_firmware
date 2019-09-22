212ef28dc47fc607c0467868b4ef76f9f2b45259
Drew Sutherland <git@drewis.fun>
2019-09-22 17:08:03 -0600

    planck: update keymap for spc/ent on left

--- keyboards/planck/keymaps/drewis/keymap.c
+++ keyboards/planck/keymaps/drewis/keymap.c
@@ -33,6 +33,7 @@ enum custom_keycodes {
 #define C_A LGUI(KC_A)
 #define SPOTLIT LGUI(KC_SPC)
 #define SCRSHT LGUI(LSFT(KC_4))
+#define KC_FUNC MO(3)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Dvorak
@@ -48,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [0] = LAYOUT_planck_grid( /* Base */
 		KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,  KC_F,   KC_G,   KC_C,   KC_R,  KC_L,   KC_BSPC,
-		CTL_ESC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,  KC_D,   KC_H,   KC_T,   KC_N,  KC_S,   CTL_MIN,
+		CTL_ESC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,  KC_D,   KC_H,   KC_T,   KC_N,  KC_S,   KC_MINS,
 		KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,  KC_B,   KC_M,   KC_W,   KC_V,  KC_Z,   KC_RSFT,
-	        KC_CAPS, KC_DEL,  KC_LALT, KC_LGUI, KC_SPC,  RAISE, LOWER,  KC_ENT, KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT),
+	        KC_FUNC, KC_DEL,  KC_LALT, KC_LGUI, KC_SPC,  KC_ENT,RAISE,  LOWER,  KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT),
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
@@ -63,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [1] = LAYOUT_planck_grid( /* L1 */
-		KC_GRV ,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, _______,
+		KC_GRV ,_______,_______,_______,KC_F4,  KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_QUES, _______,
 		_______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,  KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_PIPE,
 		_______,C_Z,    C_X,    C_C,    C_V,    C_A,   KC_PLUS,KC_EQL, KC_RABK,KC_LBRC,KC_RBRC,_______,
 		_______,_______,_______,_______,_______,_______,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END),
@@ -86,9 +87,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,_______,_______,_______,_______,_______,KC_EXLM,KC_EQL, KC_LABK,KC_LCBR,KC_RCBR,_______,
 		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______),
 [3] = LAYOUT_planck_grid( /* L3 */
+		_______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, _______,
 		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
 		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
-		_______,_______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,_______,
 		_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______),
 };
 
